0516e332b943ebe5200085fc31289b87795cde69
김현하 <hyunhaha@marketboro.com>
2023-10-04 17:21:05 +0900

    project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import { Button } from '@/components/ui/button';
+import { CardWithForm } from './components/CardWithForm';
+
+export default function Home() {
+  return (
+    <div>
+      {/* <Button>Click me</Button> */}
+      <CardWithForm />
+    </div>
+  );
+}
