60a259c8687c1c1981c318958ce7cdf435e448f2
김현하 <hyunhaha@marketboro.com>
2023-10-05 09:39:22 +0900

    add table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-// import { Button } from '@/components/ui/button';
 import { CardWithForm } from './components/CardWithForm';
 
 export default function Home() {
   return (
     <div>
-      {/* <Button>Click me</Button> */}
       <CardWithForm />
     </div>
   );
